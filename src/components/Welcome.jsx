666bb52d73b1461368cf006c786883586fc4d359
Toby Allen <tallen@twilio.com>
2019-10-03 16:54:23 +1000

    Updated Files

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form, Button, List } from 'semantic-ui-react'
+import { Form, Button } from 'semantic-ui-react'
 import tape from '../measuring-tape.png'
 
 class Welcome extends Component{
